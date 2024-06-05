abe072156338d399f6429253c9a925e698520d58
JoseCarden <josejesuscardenasjimenez@gmail.com>
2024-06-04 20:17:41 -0500

    Gateway de websocket

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,31 +1,34 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { 
+  OnGatewayDisconnect,
+  SubscribeMessage, 
+  WebSocketGateway, 
+  WebSocketServer } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { OnModuleInit } from '@nestjs/common';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'net';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatGateway implements OnModuleInit{
+@WebSocketGateway({cors: '*:*'})
+export class ChatGateway implements OnGatewayDisconnect{
 
-  constructor(private readonly chatService: ChatService) {}
+  nicknames: Map<string, string> = new Map();
 
   @WebSocketServer()
-  public server: Server;
-  
-  onModuleInit() {
+  server: Server;
 
-    //Controla cuando se conecta un cliente (socket)
-       this.server.on('connection',(socket: Socket) =>{
-
-        console.log('Bienvenido cliente: ', socket.id);
-
-        socket.on('disconnect',()=>{
-          console.log('Hasta luego: ', socket.id);
-        })
-
-       });
-
-
-
+  handleDisconnect(client: Socket) { 
+    this.server.emit('users-changed', {user: this.nicknames[client.id], event: 'left'});
+    this.nicknames.delete(client.id);
   }
 
+  @SubscribeMessage('set-nickname') 
+  setNickname(client: Socket, nickname: string) {
+    this.nicknames[client.id] = nickname;
+    this.server.emit('users-changed', {user: nickname, event: 'joined'}); 
+  }
+
+  @SubscribeMessage('add-message') 
+  addMessage(client: Socket, message) {
+    this.server.emit('message', {text: message.text, from: this.nicknames[client.id], created: new Date()});
+  }
 }
