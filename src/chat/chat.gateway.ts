cc0b68ffe6e3f72368dbb0810bcae852808da736
JoseCarden <josejesuscardenasjimenez@gmail.com>
2024-08-27 22:57:54 -0500

    Correciones originalidad: Mod 3

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -11,26 +11,26 @@ import { Socket } from 'socket.io';
 @WebSocketGateway({cors: '*:*'})
 export class ChatGateway implements OnGatewayDisconnect{
 
-  nicknames: Map<string, string> = new Map();
+  nicks: Map<string, string> = new Map();
 
   @WebSocketServer()
   server: Server;
 
   handleDisconnect(client: Socket) { 
-    this.server.emit('users-changed', {user: this.nicknames[client.id], event: 'left'});
-    this.nicknames.delete(client.id);
+    this.server.emit('users-changed', {user: this.nicks[client.id], event: 'left'});
+    this.nicks.delete(client.id);
   }
 
   @SubscribeMessage('set-nickname') 
-  setNickname(client: Socket, nickname: string) {
-    this.nicknames[client.id] = nickname;
+  ponerNick(client: Socket, nickname: string) {
+    this.nicks[client.id] = nickname;
     this.server.emit('users-changed', {user: nickname, event: 'joined'}); 
   }
 
   @SubscribeMessage('add-message') 
-  addMessage(client: Socket, message) {
+  nuevoMensaje(client: Socket, mensaje) {
     const currentDate = new Date();
-    this.server.emit('message', {text: message.text, from: this.nicknames[client.id], created: currentDate});
+    this.server.emit('message', {text: mensaje.text, from: this.nicks[client.id], created: currentDate});
     
   }
 }
